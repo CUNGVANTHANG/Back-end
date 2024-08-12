63746fa139b6adf8fcbd5749aaf8add614d37fba
CUNGVANTHANG <cungvanthang2k3@gmail.com>
2024-08-12 15:20:37 +0700

    Update

--- NestJS Theory/restfulAPI/src/main.ts
+++ NestJS Theory/restfulAPI/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public')); // js, css, images
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // view
   app.setViewEngine('ejs');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(configService.get<string>('PORT'));
 }
--- NestJS Theory/restfulAPI/src/users/schemas/user.schema.ts
+++ NestJS Theory/restfulAPI/src/users/schemas/user.schema.ts
@@ -9,7 +9,7 @@ export class User {
   email: string;
 
   @Prop({ required: true })
-  password: number;
+  password: string;
 
   @Prop()
   name: string;
